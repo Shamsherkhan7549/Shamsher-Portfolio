338030036484d8635b560372ed2530d2e0942a01
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-12-14 13:34:21 -0800

    fix: apply responsiveness on home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -11,14 +11,14 @@ import {Link} from "react-router-dom";
 const Home = () => {
   const titles = ["Frontend Developer", "Backend Developer", "Full Stack Developer", "MERN Stack Developer"]
   return (
-    <div className='flex justify-between items-center pl-25 pr-50 py-15'>
+    <div className='flex justify-between lg:flex-row flex-col-reverse items-center lg:pl-25 lg:pr-30 py-15'>
       <div className=' leading-17'>
-        <h1 className='text-5xl font-bold  leading-17 text-blue-950'>
+        <h1 className='xl:text-5xl xl:leading-17 text-3xl leading-10 font-bold text-blue-950'>
           Hi There , <br />
           I'm Shamsher <span className='text-orange-400'>Khan</span>
         </h1>
 
-        <div className="flex items-center  gap-1.5">
+        <div className="flex items-center  gap-1.5 w-70 md:w-100">
           <h2 className="text-lg md:text-2xl font-bold">
             I Am Into
           </h2>
@@ -41,7 +41,11 @@ const Home = () => {
           <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://www.linkedin.com/in/shamsher-khan-883107301/"}> <LinkedInIcon/> </Link> </p>
           <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://github.com/Shamsherkhan7549"}> <GitHubIcon/> </Link> </p>
           <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://x.com/Shamsher7549"}> <XIcon/> </Link> </p>
-          <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://www.canva.com/design/DAGswlrfFlU/OENKA-ZYn0pSRwA6r9hSBQ/edit"}> <EditDocumentIcon/> </Link> </p>
+          <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'>
+             <a href="/shamsher.resume.pdf"  download="Shamsher_Resume.png">
+              <EditDocumentIcon/>
+             </a>
+          </p>
         </div>
 
 
@@ -56,7 +60,7 @@ const Home = () => {
               exit={{ y: -10, opacity: 0 }}
               transition={{ duration: 0.5 }}
             >
-              <img className='rounded-full' src="/portfolio.png" alt="shamsher_img" />
+              <img className='rounded-full w-60 md:w-80 lg:w-100 ' src="/portfolio.png" alt="shamsher_img" />
 
             </motion.div>
           </AnimatePresence>
