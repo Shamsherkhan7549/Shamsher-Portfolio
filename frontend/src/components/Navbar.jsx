ed309144da797b9cdadb97d020f39aeb8a87cc80
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-12-14 04:54:33 -0800

    add: active feature added to nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [active, setActive] = useState("home")
   
+
+  const handleActive =(nav) => {
+    setActive(nav);
+  }
+
   return (
     <div className=' bg-blue-50 py-2 flex justify-between items-center px-10'>
 
       <img className='w-12' src="/logo.svg" alt="logo" />
 
       <ul className='gap-6 hidden sm:flex'>
-        <li className='cursor-pointer'> <Link to={'/'}> Home </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/about'}> About </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/projects'}> Projects </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/contact'}> Contact </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/skills'}> Skills </Link> </li>
+        <li className={` ${active==="home"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("home")}> <Link to={'/'}> Home </Link> </li>
+        <li className={`${active==="about"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("about")}> <Link to={'/about'}> About </Link> </li>
+        <li className={`${active==="projects"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("projects")}> <Link to={'/projects'}> Projects </Link> </li>
+        <li className={`${active==="contact"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("contact")}> <Link to={'/contact'}> Contact </Link> </li>
+        <li className={`${active==="skills"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("skills")}> <Link to={'/skills'}> Skills </Link> </li>
       </ul>
        <p className='sm:hidden duration-150 ease-in-out'>
        { 
@@ -28,11 +33,11 @@ const Navbar = () => {
 
       </p> 
       <ul className={`${isOpen ?"right-0":"right-[-140px]"} duration-200  delay-5 ease-in-out flex flex-col gap-3 sm:right-[-140px] bg-red-50 p-10 absolute top-16 `}>
-        <li className='cursor-pointer'> <Link to={'/'}> Home </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/about'}> About </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/projects'}> Projects </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/contact'}> Contact </Link> </li>
-        <li className='cursor-pointer'> <Link to={'/skills'}> Skills </Link> </li>
+        <li className={` ${active==="home"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("home")}> <Link to={'/'}> Home </Link> </li>
+        <li className={` ${active==="about"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("about")}> <Link to={'/about'}> About </Link> </li>
+        <li className={` ${active==="projects"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("projects")}> <Link to={'/projects'}> Projects </Link> </li>
+        <li className={` ${active==="contact"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("contact")}> <Link to={'/contact'}> Contact </Link> </li>
+        <li className={` ${active==="skills"? "text-blue-500 underline  underline-offset-6 decoration-2" : ""} cursor-pointer font-bold`} onClick={()=>handleActive("skills")}> <Link to={'/skills'}> Skills </Link> </li>
       </ul>
     </div>
   )
