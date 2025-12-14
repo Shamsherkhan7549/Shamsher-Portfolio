fbaa1ffc0c948e8dfa5c63d9fe9362d148919c3c
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-12-14 08:58:28 -0800

    add: home page added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { AnimatePresence } from "motion/react";
+import * as motion from "motion/react-client";
+import Typewriter from "typewriter-effect";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import XIcon from '@mui/icons-material/X';
+import EditDocumentIcon from '@mui/icons-material/EditDocument';
+import {Link} from "react-router-dom";
 
 const Home = () => {
+  const titles = ["Frontend Developer", "Backend Developer", "Full Stack Developer", "MERN Stack Developer"]
   return (
-    <div>Home</div>
+    <div className='flex justify-between items-center pl-25 pr-50 py-15'>
+      <div className=' leading-17'>
+        <h1 className='text-5xl font-bold  leading-17 text-blue-950'>
+          Hi There , <br />
+          I'm Shamsher <span className='text-orange-400'>Khan</span>
+        </h1>
+
+        <div className="flex items-center  gap-1.5">
+          <h2 className="text-lg md:text-2xl font-bold">
+            I Am Into
+          </h2>
+          <Typewriter
+            options={{
+              strings: titles,
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 50,
+              delay: 50,
+              wrapperClassName: "text-violet-700 dark:text-violet-600 text-lg md:text-2xl font-medium",
+              cursorClassName: "text-violet-700 dark:text-violet-600 text-lg md:text-2xl "
+            }}
+          />
+        </div>
+
+
+
+        <div className='flex gap-5 items-center'>
+          <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://www.linkedin.com/in/shamsher-khan-883107301/"}> <LinkedInIcon/> </Link> </p>
+          <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://github.com/Shamsherkhan7549"}> <GitHubIcon/> </Link> </p>
+          <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://x.com/Shamsher7549"}> <XIcon/> </Link> </p>
+          <p className='bg-blue-950 text-cyan-300 hover:bg-cyan-300 hover:text-white cursor-pointer h-[50px] w-[50px] rounded-full flex items-center justify-center'> <Link to={"https://www.canva.com/design/DAGswlrfFlU/OENKA-ZYn0pSRwA6r9hSBQ/edit"}> <EditDocumentIcon/> </Link> </p>
+        </div>
+
+
+      </div>
+
+      <div className='container w-100'>
+        <main className='flex justify-center align-center flex-1'>
+          <AnimatePresence mode="wait">
+            <motion.div
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <img className='rounded-full' src="/portfolio.png" alt="shamsher_img" />
+
+            </motion.div>
+          </AnimatePresence>
+        </main>
+      </div>
+    </div>
   )
 }
 
