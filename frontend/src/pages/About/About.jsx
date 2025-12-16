82dbc821e0c87f2706ac7e83e10fab2ca3048206
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-12-16 08:22:56 -0800

    add: add motion to the component

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import PersonIcon from '@mui/icons-material/Person';
+import { AnimatePresence } from "motion/react";
+import * as motion from "motion/react-client";
 
 const About = () => {
   return (
-    <div className='md:py-15 md:px-50 px-5 py-5 font-semibold text-justify'>
-      <h1 className='pb-7 sm:text-4xl text-2xl font-bold'>
-        <PersonIcon className='mr-3' fontSize="inherit"/>
-        About <span className='text-purple-800'>Me</span>
-      </h1>
-      <h2 className='font-bold sm:text-3xl text-xl '>I'm Shamsher Khan</h2>
-      <p className='font-bold sm:text-lg text-md pt-2'>Mern Stack Developer</p>
+    <AnimatePresence mode="wait ">
+      <motion.div
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -10, opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
 
-      <p className='py-5'>
-        I am a Full-Stack developer based in Pune, India. I am an Information Technology undergraduate from SPPU. I am very passionate about improving my coding skills & developing applications & websites. I build WebApps and Websites using MERN Stack. Working for myself to improve my skills. Love to build Full-Stack clones.
-      </p>
 
-      <p><span className='text-blue-600'>Email : </span> shamsher.khan7515@gmail.com </p>
-      <p className='pt-3'><span className='text-blue-600'>Place : </span> Adarsh Nagar, Delhi, 110033 </p>
+        <div className='md:py-15 md:px-50 px-5 py-5 font-semibold text-justify'>
+          <h1 className='pb-7 sm:text-4xl text-2xl font-bold'>
+            <PersonIcon className='mr-3' fontSize="inherit" />
+            About <span className='text-purple-800'>Me</span>
+          </h1>
+          <h2 className='font-bold sm:text-3xl text-xl '>I'm Shamsher Khan</h2>
+          <p className='font-bold sm:text-lg text-md pt-2'>Mern Stack Developer</p>
 
-      <button className='mt-8 py-4 px-8 cursor-pointer rounded text-lg bg-blue-800 text-white'> <a href="/shamsher.resume.pdf"  download="Shamsher_Resume.png">Resume</a></button>
+          <p className='py-5'>
+            I am a Full-Stack developer based in Pune, India. I am an Information Technology undergraduate from SPPU. I am very passionate about improving my coding skills & developing applications & websites. I build WebApps and Websites using MERN Stack. Working for myself to improve my skills. Love to build Full-Stack clones.
+          </p>
 
-    </div>
+          <p><span className='text-blue-600'>Email : </span> shamsher.khan7515@gmail.com </p>
+          <p className='pt-3'><span className='text-blue-600'>Place : </span> Adarsh Nagar, Delhi, 110033 </p>
+
+          <button className='mt-8 py-4 px-8 cursor-pointer rounded text-lg bg-blue-800 text-white'> <a href="/shamsher.resume.pdf" download="Shamsher_Resume.png">Resume</a></button>
+
+        </div>
+
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { AnimatePresence } from "motion/react";
+import * as motion from "motion/react-client";
 import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
 import PhoneIcon from '@mui/icons-material/Phone';
@@ -26,8 +28,8 @@ const Contact = () => {
       if (res.success) {
         console.log(res.data.message);
         setIsMessageSent(true)
-      }else{
-        console.log(res.data.message);   
+      } else {
+        console.log(res.data.message);
       }
 
     } catch (error) {
@@ -36,34 +38,48 @@ const Contact = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className='py-5 md:px-50 sm:px-30 px-10'>
-      <h1 className='md:text-4xl text-3xl font-bold pb-5'>Get In <span className='text-purple-500'>Touch</span></h1>
+        <AnimatePresence mode="wait ">
+          <motion.div
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+           
 
-      <div className='flex gap-2 items-center border border-gray-500 p-3 rounded '>
-        <PersonIcon />
-        <input onChange={handleContactInfo} name='name' type="text" placeholder='Name' className='outline-none leading-2 w-full bg-none  px-2 py-1 rounded' />
-      </div>
 
-      <div className='flex gap-2 items-center border border-gray-500 p-3 rounded my-3'>
-        <EmailIcon />
-        <input onChange={handleContactInfo} name='email' type="email" placeholder='Email' className='outline-none leading-2 w-full bg-none  px-2 py-1 rounded' />
-      </div>
+            <form onSubmit={handleSubmit} className='py-5 md:px-50 sm:px-30 px-10'>
+              <h1 className='md:text-4xl text-3xl font-bold pb-5'>Get In <span className='text-purple-500'>Touch</span></h1>
 
-      <div className='flex gap-2 items-center border border-gray-500 p-3 rounded '>
-        <PhoneIcon />
-        <input onChange={handleContactInfo} name='phone' type="text" placeholder='Phone' className='outline-none leading-2 w-full px-2 py-1 rounded' />
-      </div>
+              <div className='flex gap-2 items-center border border-gray-500 p-3 rounded '>
+                <PersonIcon />
+                <input onChange={handleContactInfo} name='name' type="text" placeholder='Name' className='outline-none leading-2 w-full bg-none  px-2 py-1 rounded' />
+              </div>
 
-      <div className='flex gap-2 items-start border border-gray-500 p-3 rounded my-3'>
-        <CommentIcon />
-        <textarea onChange={handleContactInfo} name='message' type="text" placeholder='Message' className='outline-none leading-6 w-full resize-none' rows={6} />
-      </div>
-      {isMessageSent === true ? <p className='text-green-400 font-semibold py-2'>Message Sent</p> : <p className='text-red-400 font-semibold py-2 '>Wait a second for submission... </p>}
-      <button className='bg-purple-500 text-white px-5 py-3 rounded font-semibold cursor-pointer'>
-        Submit <SendIcon fontSize='small' />
-      </button>
+              <div className='flex gap-2 items-center border border-gray-500 p-3 rounded my-3'>
+                <EmailIcon />
+                <input onChange={handleContactInfo} name='email' type="email" placeholder='Email' className='outline-none leading-2 w-full bg-none  px-2 py-1 rounded' />
+              </div>
 
-    </form>
+              <div className='flex gap-2 items-center border border-gray-500 p-3 rounded '>
+                <PhoneIcon />
+                <input onChange={handleContactInfo} name='phone' type="text" placeholder='Phone' className='outline-none leading-2 w-full px-2 py-1 rounded' />
+              </div>
+
+              <div className='flex gap-2 items-start border border-gray-500 p-3 rounded my-3'>
+                <CommentIcon />
+                <textarea onChange={handleContactInfo} name='message' type="text" placeholder='Message' className='outline-none leading-6 w-full resize-none' rows={6} />
+              </div>
+              {isMessageSent === true ? <p className='text-green-400 font-semibold py-2'>Message Sent</p> : <p className='text-red-400 font-semibold py-2 '>Wait a second for submission... </p>}
+              <button className='bg-purple-500 text-white px-5 py-3 rounded font-semibold cursor-pointer'>
+                Submit <SendIcon fontSize='small' />
+              </button>
+
+            </form>
+
+          </motion.div>
+        </AnimatePresence>
+
   )
 }
 
